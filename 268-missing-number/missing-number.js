1e3520c8af04588d8c7d288de76f13e5b38f4a07
Mitu kr rabha <139862128+samplemitu@users.noreply.github.com>
2025-12-27 07:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 57.3 MB (41.17%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  const n = nums.length;
+
+  let expectedSum = (n * (n+1)/2);
+  let actualSum = 0;
+
+  for(let i=0; i<nums.length; i++){
+    actualSum += nums[i];
+  }
+
+  return expectedSum - actualSum;
+
+
+};
